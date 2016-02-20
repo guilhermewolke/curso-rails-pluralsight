ff7b1c0e7c591f1970497d586edccfcdfeb7ea2b
Guilherme R. Woelke <guilherme_wolke@yahoo.com>
2016-02-20 16:12:51 -0200

    Models, rotas, e actions

--- HU/config/routes.rb
+++ HU/config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   post '/questions' => 'home#temp'
   post '/answers' => 'home#temp'
 
-  get '/question/:id' => 'home#question'
+  get '/questions/:id' => 'home#question'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
